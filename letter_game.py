ce72c95625c8678522e8635bedbd42e70c8c14c5
Andreza Feu <andrezafeu@gmail.com>
2016-08-17 19:50:52 -0400

    Guess a letter and print results accordingly

--- letter_game.py
+++ letter_game.py
@@ -4,7 +4,7 @@ words = [
 	'apple',
 	'melon',
 	'banana',
-	'organge',
+	'orange',
 	'lime',
 	'lemon',
 	'coconut',
@@ -14,10 +14,33 @@ words = [
 	]
 
 secret_word = random.choice(words)
+	
+while True:
+	start = input("\nPress enter/return to start a new game or enter Q to quit").lower()
+	if start == 'q':
+		break
+	print("\nCan you guess what is the secret word? You have 7 guesses.")
+	wrong_guesses = []
+	right_guesses = []
 
-def start_game():	
-	print("Can you guess what is the secret word?")
-	word_in_construction = "_ " * len(secret_word)
-	print(word_in_construction)
+	while len(wrong_guesses) < 7 and len(right_guesses) < len(secret_word):
+		for letter in secret_word:
+			if letter in right_guesses:
+				# end= '' allows to print multiple letters on same line
+				print(letter, end='')
+			else:
+				print("_ ", end='')
+		print('\nStrikes: {} out of 7\n'.format(len(wrong_guesses)))
 
-start_game()
+		guess = input("Pick a letter: ").lower()
+
+		if guess in secret_word:
+			right_guesses.append(guess)
+			if len(right_guesses) == len(secret_word):
+				print("Congratulations! The secret word is {}.".format(secret_word))
+				break
+		else:
+			wrong_guesses.append(guess)
+			print("This word doesn't have the letter {}.".format(guess))
+	else:
+		print("You ran out of guesses. The secret word was {}.".format(secret_word))
